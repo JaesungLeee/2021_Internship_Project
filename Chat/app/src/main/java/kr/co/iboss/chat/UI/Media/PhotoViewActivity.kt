3822c40cecab6f57efbf4d32f5b4c7f279f97a5c
JaesungLeee <viki0114@naver.com>
2021-08-05 17:06:05 +0900

    feat : Photo, Video View Activity 추가

---
+++ Chat/app/src/main/java/kr/co/iboss/chat/UI/Media/PhotoViewActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.iboss.chat.UI.Media
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.co.iboss.chat.R
+
+class PhotoViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_view)
+    }
+}
