34e6b0825bc1afcbd2353b5eeb8047038ff2932e
JaesungLeee <viki0114@naver.com>
2021-08-10 11:13:19 +0900

    feat : 멤버 초대 액티비티 생성

--- Chat/app/src/main/java/kr/co/iboss/chat/UI/GroupChannel/InviteGroupMemberActivity.kt
+++ Chat/app/src/main/java/kr/co/iboss/chat/UI/GroupChannel/InviteGroupMemberActivity.kt
@@ -2,9 +2,14 @@ package kr.co.iboss.chat.UI.GroupChannel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sendbird.android.GroupChannel
 import kr.co.iboss.chat.R
+import kr.co.iboss.chat.UI.GroupChannel.Adapter.InviteGroupMemberAdapter
 
 class InviteGroupMemberActivity : AppCompatActivity() {
+
+    private var mAdapter : InviteGroupMemberAdapter? = null
+    private var mChannel : GroupChannel? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_invite_group_member)
