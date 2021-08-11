40fc89dea17235cbe4b3816417661349393cd347
JaesungLeee <viki0114@naver.com>
2021-08-11 11:54:47 +0900

    create : WebView 프로젝트 생성

---
+++ IbossChat/app/src/main/java/kr/co/iboss/ibosschat/MainActivity.kt
@@ -0,0 +1,26 @@
+package kr.co.iboss.ibosschat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var webView = findViewById<WebView>(R.id.iboss_wV)
+        webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            setSupportMultipleWindows(true)
+            javaScriptCanOpenWindowsAutomatically = true
+        }
+        webView.webChromeClient = WebChromeClient()
+
+        webView.loadUrl("https://chat.i-boss.co.kr/login")
+    }
+
+}
