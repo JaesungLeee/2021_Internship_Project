2afa197be7dd021099a89c92d2aa62a6dd40f624
JaesungLeee <viki0114@naver.com>
2021-08-17 10:13:24 +0900

    feat : 웹뷰 연동 완료 * custom progressbar 추가

--- IbossChat/app/src/main/java/kr/co/iboss/ibosschat/UI/MainActivity.kt
+++ IbossChat/app/src/main/java/kr/co/iboss/ibosschat/UI/MainActivity.kt
@@ -1,26 +1,143 @@
 package kr.co.iboss.ibosschat.UI
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Bitmap
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
+import android.view.View
 import android.webkit.WebChromeClient
+import android.webkit.WebSettings
 import android.webkit.WebView
-import kr.co.iboss.ibosschat.R
+import android.webkit.WebViewClient
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+import kr.co.iboss.ibosschat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        var webView = findViewById<WebView>(R.id.iboss_wV)
-        webView.settings.apply {
-            javaScriptEnabled = true
-            domStorageEnabled = true
-            setSupportMultipleWindows(true)
-            javaScriptCanOpenWindowsAutomatically = true
+    companion object {
+        fun checkInternet(context: Context): Boolean {
+            var status = false
+            val connectivity = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+            if (connectivity != null) {
+                val networkInfo = connectivity.allNetworkInfo
+
+                if (networkInfo != null) {
+                    for (i in networkInfo.indices) {
+                        if (networkInfo[i].state == NetworkInfo.State.CONNECTED) {
+                            status = true
+                            break
+                        }
+                    }
+                }
+
+            }
+            return status
         }
-        webView.webChromeClient = WebChromeClient()
-
-        webView.loadUrl("https://chat.i-boss.co.kr/login")
     }
 
+    private var LOG_TAG = "MainActivity :"
+    private var EXTRA_URL = "groupChannelUrl"
+    private var URL = "https://chat.i-boss.co.kr/login"
+
+    private lateinit var mContext : Context
+    private var mChannelURL: String? = null
+    private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mWebView: WebView
+    private lateinit var mProgressBar : ProgressBar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = mBinding.root
+        setContentView(view)
+
+        mChannelURL = intent.getStringExtra(EXTRA_URL).toString()
+
+        mContext = this
+        mWebView = mBinding.ibossWV
+        mProgressBar = mBinding.progressBar
+
+        requestWebView()
+
+
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && mWebView.canGoBack()) {
+            mWebView.goBack()
+            return true
+        }
+
+        return true
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun requestWebView(){
+
+        if (checkInternet(mContext)) {
+            mWebView.loadUrl(URL)
+        }
+        else {
+            mProgressBar.visibility = View.GONE
+            mWebView.visibility = View.GONE
+
+            return
+        }
+
+        mWebView.isFocusable = true
+        mWebView.isFocusableInTouchMode = true
+        mWebView.scrollBarStyle = View.SCROLLBARS_INSIDE_OVERLAY
+        mWebView.settings.apply {
+            javaScriptEnabled = true
+            cacheMode = WebSettings.LOAD_DEFAULT
+            mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+            domStorageEnabled = true
+            setAppCacheEnabled(true)
+            databaseEnabled = true
+            setSupportMultipleWindows(false)
+        }
+
+        mWebView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, url: String?): Boolean {
+
+                Log.e(LOG_TAG, "URL : ${url!!}")
+
+                if (checkInternet(mContext)) {
+                    view.loadUrl(url)
+                }
+                else {
+                    mProgressBar.visibility = View.GONE
+                    mWebView.visibility = View.GONE
+                }
+
+                return true
+            }
+
+            override fun onPageStarted(view: WebView, url: String, favicon: Bitmap?) {
+                Log.e("onPageStarted :", "URL : $url")
+                super.onPageStarted(view, url, favicon)
+                if (mProgressBar.visibility == View.GONE) {
+                    mProgressBar.visibility = View.VISIBLE
+                }
+            }
+
+            override fun onPageFinished(view: WebView, url: String) {
+                Log.e("onPageFinished :", "URL : $url")
+                super.onPageFinished(view, url)
+
+            }
+
+            override fun onLoadResource(view: WebView, url: String) {
+                Log.e("onLoadResource :", "URL : $url")
+                super.onLoadResource(view, url)
+            }
+        }
+    }
 }
