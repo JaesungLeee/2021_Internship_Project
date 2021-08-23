38922c0fbce557139c40dc627f43bec5d18744c8
JaesungLeee <viki0114@naver.com>
2021-08-23 17:31:04 +0900

    docs : FriendsFragment 관련 Class 별 주석 추가

--- Chat/app/src/main/java/kr/co/iboss/chat/UI/Friends/FriendsFragment.kt
+++ Chat/app/src/main/java/kr/co/iboss/chat/UI/Friends/FriendsFragment.kt
@@ -12,10 +12,10 @@ import com.sendbird.android.SendBird
 import com.sendbird.android.UserListQuery
 import kr.co.iboss.chat.databinding.FragmentFriendsBinding
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FriendsFragment.newInstance] factory method to
- * create an instance of this fragment.
+/*
+ * 전체 User List를 보여주는 Fragment
+ * RecyclerView로 List를 보임
+ * 초기에 15명, 스크롤을 내리면 10명씩 추가로 보이게 구현
  */
 class FriendsFragment : Fragment() {
 
@@ -36,14 +36,13 @@ class FriendsFragment : Fragment() {
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         val binding = FragmentFriendsBinding.inflate(inflater, container, false)
         mFragFriendsBinding = binding
         return mFragFriendsBinding!!.root
     }
 
     override fun onDestroyView() {
-        mFragFriendsBinding = null
+        mFragFriendsBinding = null // Memory Leak 관련 - Binding 해제
         super.onDestroyView()
     }
 
@@ -54,14 +53,18 @@ class FriendsFragment : Fragment() {
         refresh()
     }
 
+    /*
+     * RecyclerView 초기 세팅 Method
+     */
     private fun setUpRecyclerView() {
         mLinearLayoutManager = LinearLayoutManager(mFragFriendsBinding!!.root.context)
         mFriendsListAdapter = FriendsListAdapter(mFragFriendsBinding!!.root.context)
         mFragFriendsBinding!!.friendsFragFriendsListRV.layoutManager = mLinearLayoutManager
         mFragFriendsBinding!!.friendsFragFriendsListRV.adapter = mFriendsListAdapter
-        mFragFriendsBinding!!.friendsFragFriendsListRV.addItemDecoration(DividerItemDecoration(mFragFriendsBinding!!.root.context, DividerItemDecoration.VERTICAL))
+        mFragFriendsBinding!!.friendsFragFriendsListRV.addItemDecoration(DividerItemDecoration(mFragFriendsBinding!!.root.context, DividerItemDecoration.VERTICAL)) // Item간 구분선 넣기
 
         mFragFriendsBinding!!.friendsFragFriendsListRV.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            // Scroll을 내리면 다음 10명의 Friends 리스트를 불러옴
             override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
                 if (LinearLayoutManager(mFragFriendsBinding!!.root.context).findLastVisibleItemPosition() == mFriendsListAdapter!!.itemCount - 1) {
                     loadNextFriendsList(10)
@@ -76,6 +79,12 @@ class FriendsFragment : Fragment() {
         refreshFriendsList(FREINDS_LIMIT_COUNT)
     }
 
+    /**
+     * 초기 FriendsList 불러오는 Method
+     * @param friendsCnt    요청 user
+     * @TODO createUserListQuery 대안 적용 필요
+     * @Deprecated createUserListQuery
+     */
     private fun refreshFriendsList(friendsCnt : Int) {
         mFriendListQuery = SendBird.createUserListQuery()
         mFriendListQuery!!.setLimit(friendsCnt)
@@ -90,6 +99,10 @@ class FriendsFragment : Fragment() {
         }
     }
 
+    /**
+     * Scroll 시 다음 FriendsList 불러오는 Method
+     * @param moreCnt   scroll 시 추가 요청 user
+     */
     private fun loadNextFriendsList(moreCnt : Int) {
         mFriendListQuery!!.setLimit(moreCnt)
         mFriendListQuery!!.next { friendsList, e ->
--- Chat/app/src/main/java/kr/co/iboss/chat/UI/Friends/FriendsListAdapter.kt
+++ Chat/app/src/main/java/kr/co/iboss/chat/UI/Friends/FriendsListAdapter.kt
@@ -8,6 +8,9 @@ import com.sendbird.android.User
 import kr.co.iboss.chat.Utils.ImageUtils
 import kr.co.iboss.chat.databinding.ListItemFriendsBinding
 
+/*
+ * 전체 User List를 보여주는 RecyclerView Adapter Class
+ */
 class FriendsListAdapter(private val mContext : Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private var mUsers : MutableList<User>? = null
@@ -20,6 +23,7 @@ class FriendsListAdapter(private val mContext : Context) : RecyclerView.Adapter<
     init {
         mUsers = ArrayList()
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val binding = ListItemFriendsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return FriendsListHolder(binding)
@@ -37,11 +41,21 @@ class FriendsListAdapter(private val mContext : Context) : RecyclerView.Adapter<
         mOptionClickedListener = listener
     }
 
+    /**
+     * 초기 User List 불러올 때 사용되는 Method
+     * @param users     User 리스트
+     * @see FriendsFragment.refreshFriendsList
+     */
     fun setFriendsList(users : MutableList<User>) {
         mUsers = users
         notifyDataSetChanged()
     }
 
+    /**
+     * Scroll 이후 User List를 불러올 때 사용되는 Method
+     * @param user      User 객체
+     * @see FriendsFragment.loadNextFriendsList
+     */
     fun addLast(user : User) {
         mUsers!!.add(user)
         notifyDataSetChanged()
