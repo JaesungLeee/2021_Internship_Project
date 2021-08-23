8ba7af1e19b32a2583c4de85bdfb2cc536723cb7
JaesungLeee <viki0114@naver.com>
2021-08-23 15:53:07 +0900

    feat : GroupChannel 채팅 멤버 초대 기능 구현

--- Chat/app/src/main/java/kr/co/iboss/chat/UI/GroupChannel/Adapter/InviteGroupMemberAdapter.kt
+++ Chat/app/src/main/java/kr/co/iboss/chat/UI/GroupChannel/Adapter/InviteGroupMemberAdapter.kt
@@ -1,4 +1,124 @@
 package kr.co.iboss.chat.UI.GroupChannel.Adapter
 
-class InviteGroupMemberAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.sendbird.android.User
+import kr.co.iboss.chat.Utils.ImageUtils
+import kr.co.iboss.chat.databinding.ListItemInviteGroupMembersBinding
+
+class InviteGroupMemberAdapter(private val mContext : Context, private val mIsBlockedList : Boolean, private var mShowCheckBox : Boolean) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    companion object {
+        private lateinit var mSelectedUserIdList : MutableList<String>
+    }
+
+    private var mUserList : MutableList<User>? = null
+
+    private var mInviteGroupMemberHolder : InviteGroupMemberHolder? = null
+    private var mCheckBoxStateListener : OnItemCheckedStateListener? = null
+
+    init {
+        mUserList = ArrayList()
+        mSelectedUserIdList = ArrayList()
+    }
+
+    interface OnItemCheckedStateListener {
+        fun onItemChecked(user : User, isChecked : Boolean)
+    }
+
+    fun setCheckBoxStateListener(listener : OnItemCheckedStateListener) {
+        mCheckBoxStateListener = listener
+    }
+
+    fun setUserList(users : MutableList<User>) {
+        mUserList = users
+        notifyDataSetChanged()
+    }
+
+    fun setShowCheckBox(showCheckBox: Boolean) {
+        mShowCheckBox = showCheckBox
+        if (mInviteGroupMemberHolder != null) {
+            mInviteGroupMemberHolder!!.setShowCheckBox(showCheckBox)
+        }
+        notifyDataSetChanged()
+    }
+
+    fun addLast(user : User) {
+        mUserList!!.add(user)
+        notifyDataSetChanged()
+    }
+
+
+    private fun isCheckBoxSelected(user : User) : Boolean {
+        return mSelectedUserIdList.contains(user.userId)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val binding = ListItemInviteGroupMembersBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return InviteGroupMemberHolder(binding, mIsBlockedList, mShowCheckBox)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as InviteGroupMemberHolder).bindViews(mContext, mUserList!![position], isCheckBoxSelected(mUserList!![position]), mCheckBoxStateListener!!)
+    }
+
+    override fun getItemCount(): Int {
+        return mUserList!!.size
+    }
+
+    private inner class InviteGroupMemberHolder(val binding: ListItemInviteGroupMembersBinding, private val mIsBlockedList : Boolean, private var mShowCheckBox: Boolean) : RecyclerView.ViewHolder(binding.root){
+
+        init {
+            this.setIsRecyclable(false)
+        }
+
+        fun setShowCheckBox(showCheckBox: Boolean) {
+            mShowCheckBox = showCheckBox
+        }
+
+        fun bindViews(context: Context, user: User, isCheckBoxSelected: Boolean, listener: OnItemCheckedStateListener) {
+            setUserAttribute(context, user)
+            setBlockImageView()
+            setCheckBoxStatus(isCheckBoxSelected)
+            setSelectedUserIdList(user, listener)
+        }
+
+        private fun setUserAttribute(context: Context, user: User) {
+            binding.inviteGroupMemberMemberNickNameTV.text = user.nickname
+
+            ImageUtils.displayRoundImageFromUrl(context, user.profileUrl, binding.inviteGroupMemberMemberProfileIV)
+        }
+
+        private fun setBlockImageView() {
+            if (mIsBlockedList) binding.inviteGroupMemberMemberBlockedIV.visibility = View.VISIBLE
+            else binding.inviteGroupMemberMemberBlockedIV.visibility = View.GONE
+        }
+
+        private fun setCheckBoxStatus(isCheckBoxSelected: Boolean) {
+            if (mShowCheckBox) binding.inviteGroupMemberCheckBox.visibility = View.VISIBLE
+            else binding.inviteGroupMemberCheckBox.visibility = View.GONE
+
+            binding.inviteGroupMemberCheckBox.isChecked = isCheckBoxSelected
+
+            if (mShowCheckBox) {
+                binding.inviteGroupMemberItemContainer.setOnClickListener {
+                    if (mShowCheckBox) {
+                        binding.inviteGroupMemberCheckBox.isChecked = !binding.inviteGroupMemberCheckBox.isChecked
+                    }
+                }
+            }
+        }
+
+        private fun setSelectedUserIdList(user : User, listener: OnItemCheckedStateListener) {
+            binding.inviteGroupMemberCheckBox.setOnCheckedChangeListener { buttonView, isChecked ->
+                listener.onItemChecked(user, isChecked)
+
+                if (isChecked) mSelectedUserIdList.add(user.userId)
+                else mSelectedUserIdList.remove(user.userId)
+            }
+        }
+
+    }
 }
--- Chat/app/src/main/java/kr/co/iboss/chat/UI/GroupChannel/InviteGroupMemberActivity.kt
+++ Chat/app/src/main/java/kr/co/iboss/chat/UI/GroupChannel/InviteGroupMemberActivity.kt
@@ -1,17 +1,133 @@
 package kr.co.iboss.chat.UI.GroupChannel
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.sendbird.android.GroupChannel
-import kr.co.iboss.chat.R
+import com.sendbird.android.SendBird
+import com.sendbird.android.User
+import com.sendbird.android.UserListQuery
 import kr.co.iboss.chat.UI.GroupChannel.Adapter.InviteGroupMemberAdapter
+import kr.co.iboss.chat.databinding.ActivityInviteGroupMemberBinding
 
 class InviteGroupMemberActivity : AppCompatActivity() {
+    companion object {
+        private val EXTRA_CHANNEL_URL = "EXTRA_CHANNEL_URL"
+        private val MEMBERS_LIMIT_COUNT = 15
+    }
+
+    private lateinit var binding : ActivityInviteGroupMemberBinding
 
     private var mAdapter : InviteGroupMemberAdapter? = null
-    private var mChannel : GroupChannel? = null
+    private var mChannelURL : String? = null
+
+    private var mMemberListQuery : UserListQuery? = null
+    private var mSelectedUserIdList : MutableList<String>? = null
+
+
+    init {
+        this.mSelectedUserIdList = ArrayList()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_invite_group_member)
+
+        binding = ActivityInviteGroupMemberBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        mChannelURL = intent.getStringExtra(EXTRA_CHANNEL_URL)
+
+        setUpAdapter()
+        setUpRecyclerView()
+        loadFirstUserList(MEMBERS_LIMIT_COUNT)
+        buttonListener()
     }
+
+    private fun setUpAdapter() {
+        mAdapter = InviteGroupMemberAdapter(this, false, true)
+        mAdapter!!.setCheckBoxStateListener(object : InviteGroupMemberAdapter.OnItemCheckedStateListener {
+            override fun onItemChecked(user: User, isChecked: Boolean) {
+                if (isChecked) mSelectedUserIdList!!.add(user.userId)
+                else mSelectedUserIdList!!.remove(user.userId)
+
+                // 선택된 사람이 있을 때 초대버튼 활성화
+                binding.groupMemberInviteBtn.isEnabled = mSelectedUserIdList!!.size > 0
+            }
+        })
+    }
+
+    private fun setUpRecyclerView() {
+        val mLayoutManager = LinearLayoutManager(this)
+        binding.inviteGroupMemberMemberListRV.layoutManager = mLayoutManager
+        binding.inviteGroupMemberMemberListRV.adapter = mAdapter
+        binding.inviteGroupMemberMemberListRV.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+
+        binding.inviteGroupMemberMemberListRV.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                if (mLayoutManager.findLastVisibleItemPosition() == mAdapter!!.itemCount - 1) {
+                    loadNextUserList(10)
+                }
+            }
+        })
+    }
+
+    private fun buttonListener() {
+        binding.groupMemberInviteNavigateBeforeBtn.setOnClickListener {
+            finish()
+        }
+        binding.groupMemberInviteBtn.setOnClickListener {
+            if (mSelectedUserIdList!!.size > 0) inviteGroupMemberWithUserId()
+            else Toast.makeText(this, "초대할 멤버를 선택해주세요.", Toast.LENGTH_LONG).show()
+        }
+        binding.groupMemberInviteBtn.isEnabled = false
+    }
+
+    private fun inviteGroupMemberWithUserId() {
+        GroupChannel.getChannel(mChannelURL, GroupChannel.GroupChannelGetHandler { groupChannel, e ->
+            if (e != null) {
+                return@GroupChannelGetHandler
+            }
+
+            groupChannel.inviteWithUserIds(mSelectedUserIdList, GroupChannel.GroupChannelInviteHandler { e ->
+                if (e != null) {
+                    return@GroupChannelInviteHandler
+                }
+
+                finish()
+            })
+        })
+    }
+    private fun loadFirstUserList(size: Int) {
+        mMemberListQuery = SendBird.createUserListQuery()
+
+        mMemberListQuery!!.setLimit(size)
+        mMemberListQuery!!.next(UserListQuery.UserListQueryResultHandler { list, e ->
+            if (e != null) {
+                return@UserListQueryResultHandler
+            }
+
+            mAdapter!!.setUserList(list)
+        })
+    }
+
+    private fun loadNextUserList(size : Int) {
+        mMemberListQuery!!.setLimit(size)
+
+        mMemberListQuery!!.next(UserListQuery.UserListQueryResultHandler { list, e ->
+            if (e != null) {
+                return@UserListQueryResultHandler
+            }
+
+            for (user in list) {
+                mAdapter!!.addLast(user)
+            }
+        })
+    }
+
+
+
 }
