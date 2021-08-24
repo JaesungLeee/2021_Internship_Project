af5b1873d6ed9dc6a52bb3e2c022754ae2863e44
JaesungLeee <viki0114@naver.com>
2021-08-24 17:04:05 +0900

    docs : ConnectionUtils 관련 주석 추가

--- Chat/app/src/main/java/kr/co/iboss/chat/Utils/ConnectionUtils.kt
+++ Chat/app/src/main/java/kr/co/iboss/chat/Utils/ConnectionUtils.kt
@@ -3,24 +3,35 @@ package kr.co.iboss.chat.Utils
 import com.sendbird.android.SendBird
 import kr.co.iboss.chat.BaseApplication
 
+/*
+ * Connection과 관련된 Method들을 관리하는 Utils File
+ * Login, Logout, ConnectionHandler ..
+ */
 object ConnectionUtils {
 
+    /**
+     * 로그인 상태 판별
+     * @return T/F
+     */
     fun isLogin() : Boolean {
         return PreferencesUtils(BaseApplication.instance.context()).getConnected()
     }
 
+    /* SendBird 서버와의 연결을 설정하는 Method */
     fun login(userId : String, handler : SendBird.ConnectHandler?) {
         SendBird.connect(userId) { user, e ->
             handler?.onConnected(user, e)
         }
     }
 
+    /* SendBird 서버와의 연결을 해지하는 Method */
     fun logout(handler : SendBird.DisconnectHandler?) {
         SendBird.disconnect {
             handler?.onDisconnected()
         }
     }
 
+    /* SendBird 서버 Connection Handler Method */
     fun addConnectionManagementHandler(handlerId : String, handler: ConnectionManagementHandler?) {
         SendBird.addConnectionHandler(handlerId, object : SendBird.ConnectionHandler {
             override fun onReconnectStarted() {
@@ -40,6 +51,7 @@ object ConnectionUtils {
             handler?.onConnected(false)
         }
 
+        // 재 연결 설정
         else if (SendBird.getConnectionState() == SendBird.ConnectionState.CLOSED) {
             val userId = PreferencesUtils(BaseApplication.instance.context()).getUserId()
             SendBird.connect(userId) { user, e ->
@@ -51,10 +63,12 @@ object ConnectionUtils {
         }
     }
 
+    /* SendBird 서버 Remove Connection Handler Method */
     fun removeConnectionManagementHandler(handlerId: String) {
         SendBird.removeConnectionHandler(handlerId)
     }
 
+    /* 다른 File에서 참조할 수 있도록 설정한 Interface */
     interface ConnectionManagementHandler {
         fun onConnected(reconnect : Boolean)
     }
