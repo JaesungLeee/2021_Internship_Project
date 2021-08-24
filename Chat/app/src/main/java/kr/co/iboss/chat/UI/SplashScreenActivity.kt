de66b11c62db5b750576ff82ed9b1980fa3def39
JaesungLeee <viki0114@naver.com>
2021-08-24 11:05:37 +0900

    docs : SplashScreenActivity 관련 주석 추가

--- Chat/app/src/main/java/kr/co/iboss/chat/UI/SplashScreenActivity.kt
+++ Chat/app/src/main/java/kr/co/iboss/chat/UI/SplashScreenActivity.kt
@@ -14,10 +14,6 @@ import kr.co.iboss.chat.Utils.PreferencesUtils
 
 class SplashScreenActivity : AppCompatActivity() {
 
-    companion object {
-        private val INTENT_USER_ID              = "INTENT_USER_ID"
-    }
-
     lateinit var handler : Handler
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,10 +30,12 @@ class SplashScreenActivity : AppCompatActivity() {
         }, 3000)
     }
 
+    /* 자동 로그인 처리 */
     private fun autoLogin() {
         val preference = PreferencesUtils(this)
         val userId = preference.getUserId()
 
+        /* 로그인 상태이고 저장되어있는 userId가 있을 때 */
         if (ConnectionUtils.isLogin() && !TextUtils.isEmpty(userId)) {
             ConnectionUtils.login(userId) { user, e ->
                 if (e != null) {
@@ -45,7 +43,7 @@ class SplashScreenActivity : AppCompatActivity() {
                 }
 
                 Log.e("AUTOLOGIN", "SUCCESS")
-                startActivity(handleNextIntent())
+                startActivity(handleNextIntent())   // handleNextIntent( )에서 login 상태 검사
                 finish()
             }
         }
@@ -55,11 +53,11 @@ class SplashScreenActivity : AppCompatActivity() {
         }
     }
 
-    /* 로그인 상태일 때 groupChannelUrl 전달 */
+    /* SplashScreen 이후 다음 UI 전환 처리 */
     private fun handleNextIntent(): Intent {
         if (ConnectionUtils.isLogin()) {
-            val intent = Intent(this, MainActivity::class.java)
-            if (getIntent().hasExtra("groupChannelUrl")) {
+            val intent = Intent(this, MainActivity::class.java)     // 로그인 상태일 때 MainActivity로 전환
+            if (getIntent().hasExtra("groupChannelUrl")) {      // 푸시 메시지 클릭시 intent에 groupChannelUrl 담아 같이 전달
                 intent.putExtra("groupChannelUrl", getIntent().getStringExtra("groupChannelUrl"))
             }
             return intent
