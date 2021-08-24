af8c611ea23ee11ac538660f360efbf6b669dd86
JaesungLeee <viki0114@naver.com>
2021-08-24 17:07:14 +0900

    docs : DateUtils 관련 주석 추가

--- Chat/app/src/main/java/kr/co/iboss/chat/Utils/DateUtils.kt
+++ Chat/app/src/main/java/kr/co/iboss/chat/Utils/DateUtils.kt
@@ -3,23 +3,24 @@ package kr.co.iboss.chat.Utils
 import java.text.SimpleDateFormat
 import java.util.*
 
+/* 시간, 날짜를 Formatting 하는 Utils File */
 object DateUtils {
 
-    // 밀리초 -> HH:mm 형태로 포맷
+    /* 밀리초 -> HH:mm 형태로 포맷 */
     fun formatTime(timeInMilliSec : Long) : String {
         val dateFormat = SimpleDateFormat("HH:mm", Locale.KOREAN)
         dateFormat.timeZone = TimeZone.getTimeZone("Asia/Seoul")
         return dateFormat.format(timeInMilliSec)
     }
 
-    // date month 형식으로 ('July 16')
+    /* date month 형식으로 ('July 16') */
     fun formatDate(timeInMilliSec: Long) : String {
         val dateFormat = SimpleDateFormat("MMMM dd일", Locale.KOREAN)
         dateFormat.timeZone = TimeZone.getTimeZone("Asia/Seoul")
         return dateFormat.format(timeInMilliSec)
     }
 
-    // 디바이스 시간 기준으로 date가 오늘인지 아닌지 : Boolean
+    /* 디바이스 시간 기준으로 date가 오늘인지 아닌지 : Boolean */
     fun isToday(timeInMilliSec: Long) : Boolean {
         val dateFormat = SimpleDateFormat("yyyyMMdd", Locale.KOREAN)
         dateFormat.timeZone = TimeZone.getTimeZone("Asia/Seoul")
@@ -27,7 +28,7 @@ object DateUtils {
         return date == dateFormat.format(System.currentTimeMillis())
     }
 
-    // 시간 혹은 날짜를 return
+    /* 시간 혹은 날짜를 return */
     fun formatDateTime(timeInMilliSec: Long) : String {
         return if (isToday(timeInMilliSec)) {
             formatTime(timeInMilliSec)
